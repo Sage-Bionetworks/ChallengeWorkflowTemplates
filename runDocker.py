766feeccadc2981acec6b3c601a6d2576f18c0fc
thomasyu888 <thomasyu888@gmail.com>
2018-10-08 23:25:47 -0700

    Docker run

--- runDocker.py
+++ runDocker.py
@@ -3,6 +3,13 @@ import synapseclient
 import json
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--submissionId", required=True, help="Submission Id")
+parser.add_argument("-p", "--dockerRepository", required=True, help="Docker Repository")
+parser.add_argument("-d", "--dockerDigest", required=True, help="Docker Digest")
+parser.add_argument("-i", "--inputFile", required=True, help="Docker Digest")
+
+args = parser.parse_args()
 
 # inputs:
 #   - id: submissionId
@@ -15,21 +22,20 @@ import argparse
 #     type: string
 #   - id: dockerAuth
 #     type: string
+
 client = docker.from_env()
 #Add docker.config file
-dockerImage = dockerRepository + "@" + dockerDigest
+dockerImage = args.dockerRepository + "@" + args.dockerDigest
 
 #These are the volumes that you want to mount onto your docker container
-OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),submission.id)
-TESTDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),'test-data')
-CACHE = "/.synapseCache"
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),args.submissionId)
+TESTDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),args.inputFile)
 #These are the locations on the docker that you want your mounted volumes to be + permissions in docker (ro, rw)
 #It has to be in this format '/output:rw'
 MOUNTED_VOLUMES = {OUTPUT_DIR:'/output:rw',
-                   TESTDATA_DIR:'/test-data:ro',
-                   CACHE:'/.synapseCache:ro'}
+                   TESTDATA_DIR:'/input:ro'}
 #All mounted volumes here in a list
-ALL_VOLUMES = [OUTPUT_DIR,TESTDATA_DIR,CACHE]
+ALL_VOLUMES = [OUTPUT_DIR,TESTDATA_DIR]
 #Mount volumes
 volumes = {}
 for vol in ALL_VOLUMES:
@@ -38,7 +44,7 @@ for vol in ALL_VOLUMES:
 #TODO: Look for if the container exists already, if so, reconnect 
 errors = None
 try:
-    container = client.containers.run(dockerImage, detach=True,volumes = volumes, name=submissionId, network_disabled=True)
+    container = client.containers.run(dockerImage, detach=True,volumes = volumes, name=args.submissionId, network_disabled=True)
 except docker.errors.APIError as e:
     container = None
     errors = str(e) + "\n"
